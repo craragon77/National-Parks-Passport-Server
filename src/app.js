fa28a52f993c1dd3869d6f8881958d44b68b4a50
Chris Aragon <aragonchristopherray@gmail.com>
2020-06-29 15:52:55 -0400

    cleaning up the code

--- src/app.js
+++ src/app.js
@@ -5,14 +5,9 @@ const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
 const {CLIENT_ORIGIN} = require('./config');
-const ParkService = require('./Service-Repo/ParkService');
 const ParkRouter = require('./Router-Repo/ParksRouter');
-const UserService = require('./Service-Repo/UserService');
 const knex = require('knex');
-const StampBookService = require('./Service-Repo/StampbookService');
-const BucketListService = require('./Service-Repo/BucketListService');
 const UserRouter = require('./Router-Repo/UserRouter');
-const StampRouter = require('./Router-Repo/StampBookRouter');
 const StampBookRouter = require('./Router-Repo/StampBookRouter');
 const BucketListRouter = require('./Router-Repo/BucketListRouter');
 
@@ -47,25 +42,6 @@ app.use('/stampbook', StampBookRouter)
 
 app.use('/bucketlist', BucketListRouter)
 
-/*app.get('/bucketlist', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    BucketListService.getAllBucketList(knexInstance)
-        .then(bucket => {
-            res.json(bucket)
-        })
-        .catch(next)
-})
-
-app.get('/bucketlist/:bucketlistId', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    const bucketlist_id = req.params.bucketlistId
-    BucketListService.getBucketListById(knexInstance, bucketlist_id)
-        .then(id => {
-            res.json(id)
-        })
-        .catch(next)
-})
-*/
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
